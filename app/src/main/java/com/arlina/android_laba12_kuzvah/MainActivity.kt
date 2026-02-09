430766591382535ab5d3e916a193826f1ab518c1
Artur <only.up.ak@gmail.com>
2026-02-09 17:27:05 +0300

    Step 7: Basic Grade Clicker app completed

--- app/src/main/java/com/arlina/android_laba12_kuzvah/MainActivity.kt
+++ app/src/main/java/com/arlina/android_laba12_kuzvah/MainActivity.kt
@@ -3,14 +3,45 @@ package com.arlina.android_laba12_kuzvah
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.runtime.remember
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.arlina.android_laba12_kuzvah.data.Datasource
+import com.arlina.android_laba12_kuzvah.model.Grade
 import com.arlina.android_laba12_kuzvah.ui.theme.Android_Laba12_KuzvahTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +50,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Android_Laba12_KuzvahTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    GradeClickerApp(grades = Datasource.gradeList)
                 }
             }
         }
@@ -31,17 +61,100 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GradeClickerApp(grades: List<Grade>){
+    var points by remember { mutableStateOf(0) }
+    var clicks by remember { mutableStateOf(0) }
+    val currentGrade = determineGradeToShow(grades,points)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+        Image(
+            painter = painterResource(currentGrade.imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.image_size))
+                .clickable {
+                    points += currentGrade.pointsPerClick
+                    clicks++
+                },
+            contentScale = ContentScale.Fit
+        )
+        TransactionInfo(
+            points = points,
+            clicks = clicks
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Android_Laba12_KuzvahTheme {
-        Greeting("Android")
+fun TransactionInfo(points: Int,
+                    clicks: Int,
+                    modifier: Modifier=Modifier)
+{
+    Column(
+        modifier =modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.points_earned),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = points.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.total_clicks),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = clicks.toString(),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+fun determineGradeToShow(
+    grades: List<Grade>,
+    points: Int
+) : Grade{
+    var  gradeToShow = grades.first()
+    for (grade in grades){
+        if (points >= grade.threshold){
+            gradeToShow = grade
+        } else{
+            break
+        }
+    }
+    return gradeToShow
+}
+
+
+@Preview
+@Composable
+fun GradeClickerPreview(){
+    Android_Laba12_KuzvahTheme{
+        GradeClickerApp(grades = Datasource.gradeList)
     }
 }
